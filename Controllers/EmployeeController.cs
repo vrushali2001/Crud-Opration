87e632a76c0055076f3dbb8cf051abd619e97c96
vrushali2001 <vrushali.navale2001@gmail.com>
2024-05-11 15:33:34 +0530

    CRUD

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,73 @@
+﻿using dbFirst.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dbFirst.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //injecting a dependency in controller 
+        CrudVContext _db;
+        public EmployeeController(CrudVContext db)
+        {
+            _db = db;
+        }
+        
+        public IActionResult Index()
+        {
+          var e=  _db.Employees.ToList();
+            return View(e);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+          return View();  
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee e)
+        {
+            _db.Employees.Add(e);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+           var r= _db.Employees.Find(id);
+            return View(r);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee o) 
+        {
+            _db.Employees.Update(o);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+
+        public IActionResult Delete(int id) 
+        {
+           var e= _db.Employees.Find(id);
+            return View(e);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult Delete_c(int id)
+        {
+           var d= _db.Employees.Find(id);
+               _db.Employees.Remove(d);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+           var e= _db.Employees.Find(id);
+            return View(e);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using dbFirst.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<CrudVContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CrudV")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Employee}/{action=Index}/{id?}");
+
+app.Run();
